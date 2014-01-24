0a9a2996eba7c504e794147cbcb3b605ed262664
Tyler Hartland <tyler.hartland@code42.com>
2014-01-24 16:55:24 -0600

    update specs

--- spec/lib/when-do_spec.rb
+++ spec/lib/when-do_spec.rb
@@ -9,22 +9,26 @@ def key
 end
 
 describe When do
+  before do
+    When.stub(:schedule_key).and_return(key)
+  end
+
   after(:all) do
     redis.del(key)
   end
 
   describe '#schedule' do
     it 'adds data to the schedules hash in redis' do
-      When.schedule('test_schedule', Object, '* * * * *', [], key)
-      expect(redis.hget(key, 'test_schedule')).to eq "{\"class\":\"Object\",\"cron\":\"* * * * *\",\"args\":[]}"
+      When.schedule('test_schedule', Object, '* * * * *', 'arg1', 'arg2')
+      expect(redis.hget(key, 'test_schedule')).to eq "{\"class\":\"Object\",\"cron\":\"* * * * *\",\"args\":[\"arg1\",\"arg2\"]}"
     end
   end
 
   describe '#unschedule' do
     it 'removes data from the schedules hash in redis' do
-      When.schedule('test_schedule', Object, '* * * * *', [], key)
+      When.schedule('test_schedule', Object, '* * * * *')
       expect(redis.hget(key, 'test_schedule')).to eq "{\"class\":\"Object\",\"cron\":\"* * * * *\",\"args\":[]}"
-      When.unschedule('test_schedule', key)
+      When.unschedule('test_schedule')
       expect(redis.hget(key, 'test_schedule')).to be_nil
     end
   end
